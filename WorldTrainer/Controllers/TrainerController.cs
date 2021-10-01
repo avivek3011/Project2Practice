5e9e8a4a2b99636f0c82f4b88b7d45167bf77041
SaadMansuri3 <saadmansurithree@gmail.com>
2021-10-01 17:22:01 +0530

    working registration and login

---
+++ WorldTrainer/Controllers/TrainerController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLayer.Repositories;
+using WorldTrainer.Models;
+
+namespace WorldTrainer.Controllers
+{
+
+
+    public class TrainerController : Controller
+    {
+        ITrainerRepo repo;
+
+        const string Username = "";
+        const string TrainerId = "";
+
+        public TrainerController(ITrainerRepo _repo)
+        {
+                repo = _repo;
+        }
+
+        // GET: TrainerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TrainerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TrainerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TrainerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TrainerModel trainer)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    repo.Add(Mapper.Map(trainer), Mapper.MapDetails(trainer));
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(TrainerModel tm)
+        {
+            var data = repo.Login(Mapper.MapLogin(tm));
+            if (data != null)
+            {
+                HttpContext.Session.SetString(TrainerId, data.Id.ToString());
+                //HttpContext.Session.SetString(Username, data.Username);
+
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ViewBag.Message = "Username & Password is not Correct";
+                return View();
+            }
+        }
+
+        public IActionResult Display()
+        {
+            var id = HttpContext.Session.GetString(TrainerId);
+            int Uid = Convert.ToInt32(id);
+            var data = Mapper.Map(repo.Display(Uid));
+            return View(data);
+        }
+
+
+        // GET: TrainerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TrainerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TrainerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TrainerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WorldTrainer/Models/Mapper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldTrainer.Models
+{
+    public class Mapper
+    {
+        public static DataLayer.Entities.Trainer Map(Models.TrainerModel trainer)
+        {
+            return new DataLayer.Entities.Trainer()
+            {
+                Id = trainer.id,
+                Name = trainer.name,
+                Username = trainer.username,
+                Password = trainer.password,
+                Age = trainer.age,
+                Contact = trainer.contact,
+                Email = trainer.email,
+                Gender = trainer.gender,
+            };
+        }
+
+        public static Models.TrainerModel Map(DataLayer.Entities.Trainer trainer)
+        {
+            return new Models.TrainerModel()
+            {
+                id = trainer.Id,
+                name = trainer.Name,
+                username = trainer.Username,
+                password = trainer.Password,
+                age = trainer.Age,
+                contact = trainer.Contact,
+                email = trainer.Email,
+                gender = trainer.Gender,
+            };
+        }
+
+
+        public static DataLayer.Entities.TrainerDetails MapDetails(Models.TrainerModel trainer)
+        {
+            return new DataLayer.Entities.TrainerDetails()
+            {
+                Experience = trainer.experience,
+                Qualification = trainer.qualification
+            };
+        }
+
+
+        public static DataLayer.Entities.Trainer MapLogin(TrainerModel trainer)
+        {
+            return new DataLayer.Entities.Trainer()
+            {
+                Username = trainer.username,
+                Password = trainer.password
+            };
+        }
+
+        public static IEnumerable<Models.TrainerModel> Map(IEnumerable<DataLayer.Entities.Trainer> trainer) => trainer.Select(Map);
+    }
+}
